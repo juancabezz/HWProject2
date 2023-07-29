051d202725285f636f4b0fb75a2ebb2f7e31f2bb
juancabeza <juancabeza@localhost>
2023-07-28 23:11:46 -0400

    Initial Commit

---
+++ src/travel/NextTrip.java
@@ -0,0 +1,11 @@
+package travel;
+
+public class NextTrip {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("On my next vacation I will go to Egypt...");
+		System.out.println("I want to visit the pyramids.");
+	}
+
+}
